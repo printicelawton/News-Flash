c4454389254eaabcd0ff638cc73133ebb70e6d85
Jonathan Rein <jrein1296@gmail.com>
2021-03-25 10:12:01 -0400

    Adding notes about available NY Times API calls. Key included.

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+//   NY Times API Key = eotEjLHYnnth5tmNvCO16GEf0pwkAoI0  our app id = 7d9e7f1b-850f-47b5-b521-8bbf7ebf162c
+
+//  NY Times Most Popular call 
+// https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=eotEjLHYnnth5tmNvCO16GEf0pwkAoI0
+
+// NY Times top stories (by topic, others are available.  See https://developer.nytimes.com/docs/top-stories-product/1/overview)
+// https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=eotEjLHYnnth5tmNvCO16GEf0pwkAoI0
+// https://api.nytimes.com/svc/topstories/v2/home.json?api-key=eotEjLHYnnth5tmNvCO16GEf0pwkAoI0
+// https://api.nytimes.com/svc/topstories/v2/science.json?api-key=eotEjLHYnnth5tmNvCO16GEf0pwkAoI0
+// https://api.nytimes.com/svc/topstories/v2/us.json?api-key=eotEjLHYnnth5tmNvCO16GEf0pwkAoI0
+// https://api.nytimes.com/svc/topstories/v2/world.json?api-key=eotEjLHYnnth5tmNvCO16GEf0pwkAoI0
+
+// NY Times News Wire (Others are available by topic.  See https://developer.nytimes.com/docs/timeswire-product/1/overview)
+// https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=eotEjLHYnnth5tmNvCO16GEf0pwkAoI0
