891b8ca5ec584dc88539c3c33edb01a7782ee9a4
Lisa Pessin <lisapessin@gmail.com>
2021-03-26 12:45:19 -0400

    added nytimes API & category picker

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,29 @@
-//   NY Times API Key = eotEjLHYnnth5tmNvCO16GEf0pwkAoI0  our app id = 7d9e7f1b-850f-47b5-b521-8bbf7ebf162c
 
-//  NY Times Most Popular call 
-// https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=eotEjLHYnnth5tmNvCO16GEf0pwkAoI0
+var categoryChoice = document.getElementById("category");
+var submitBtn = document.getElementById("submit");
 
-// NY Times top stories (by topic, others are available.  See https://developer.nytimes.com/docs/top-stories-product/1/overview)
-// https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=eotEjLHYnnth5tmNvCO16GEf0pwkAoI0
-// https://api.nytimes.com/svc/topstories/v2/home.json?api-key=eotEjLHYnnth5tmNvCO16GEf0pwkAoI0
-// https://api.nytimes.com/svc/topstories/v2/science.json?api-key=eotEjLHYnnth5tmNvCO16GEf0pwkAoI0
-// https://api.nytimes.com/svc/topstories/v2/us.json?api-key=eotEjLHYnnth5tmNvCO16GEf0pwkAoI0
-// https://api.nytimes.com/svc/topstories/v2/world.json?api-key=eotEjLHYnnth5tmNvCO16GEf0pwkAoI0
 
-// NY Times News Wire (Others are available by topic.  See https://developer.nytimes.com/docs/timeswire-product/1/overview)
-// https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=eotEjLHYnnth5tmNvCO16GEf0pwkAoI0
+var nyTimesAPIKey = "VqGXtQf3PtyfZrtXwxjc54VEnZhc9QnR"
+var newsAPIKey = "850cb5d6549542ee848e80d5d1910b21"
+
+
+
+function nyTimesTopStoriesAPI() {
+    var categoryValue = categoryChoice.options[categoryChoice.selectedIndex].value;
+    var requestTopStories = "https://api.nytimes.com/svc/topstories/v2/" + categoryValue + ".json?api-key=" + nyTimesAPIKey;
+    console.log(categoryChoice.options[categoryChoice.selectedIndex].value);
+
+    fetch(requestTopStories)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+}
+
+submitBtn.addEventListener("click", function() {
+    event.preventDefault();
+    nyTimesTopStoriesAPI();
+    console.log("ive been clicked 1");
+})
