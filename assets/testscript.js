8412d1b0901e61a6cc86397acab8d5754d5fa591
Printice Lawton <printicelaw10@gmail.com>
2021-03-30 19:50:15 -0400

    Test

---
+++ assets/testscript.js
@@ -0,0 +1,19 @@
+//look up MDN web doc for more info on Fetch 
+// You can get a GET, POST or FETCH 
+
+//Step one: Call Fetch
+//Step two: Deal with the response (this is the promise, it happens Asynchronously)
+//Step three:  The reponse is a stream of data (text, blob, json - a format for storing data)
+//Step four:  Complete data stream
+
+
+
+//async and await function //
+//async function catchRainbow () {
+ //   const response = await fetch ('HTPPS: API'); 
+  //  const json = await response.json ();
+   // document.getElementById('API')
+//}
+
+
+// Where the ISS is right now 
